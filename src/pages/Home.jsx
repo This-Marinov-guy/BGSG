67668fef9e526f16b30170473393dff7d679a1da
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-23 14:50:35 +0200

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = (props) => {
     {
       textPosition: "text-center",
       category: "",
-      title: "Bulgarian Hello Groningen",
+      title: "Bulgarian Society Groningen",
       description: "",
       buttonText: user.token ? "Go To Profile" : "Become a Member",
       style: user.token ? " rn-btn-reverse-green" : " btn-primary-color",
