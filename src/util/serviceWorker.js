69a0e76bb68f69bc1948b3682813066b8302b27c
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-23 13:52:30 +0200

    modified:   src/util/serviceWorker.js 	modified:   public/service-worker.js

--- src/util/serviceWorker.js
+++ src/util/serviceWorker.js
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+const CACHE_NAME = `bgsg-static-v${APP_VERSION}`;
+
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
