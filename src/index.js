df68568e6dae77e1b2f4d52e50a2c63251de7d8c
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-24 11:16:36 +0200

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -55,50 +55,28 @@ const Success = lazy(() => import("./pages/redirects/Success"));
 const Fail = lazy(() => import("./pages/redirects/Fail"));
 
 const openSocietyEvents = [
-  {
-    title: "Freedom Fest",
-    description: "National day of Bulgaria",
-    bgImage: "4",
-    date: "3.3.2023",
-    time: "20:00",
-    //Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
-    correctedDate: "",
-    correctedTime: "",
-    where: "Business Hall",
-    entry: 8,
-    memberEntry: 5,
-    price_id: "price_1MwA1eIOw5UGbAo12l33bqnE",
-    memberPrice_id: "price_1N0LPTIOw5UGbAo1UsHKyBQh",
-    text: [
-      "Wild party",
-      "We will provide drinks and snacks for our socity. Music and great spirit will crowd the dance floor as we promisethis will be an unforgetable experience that will be talked about for weeks after! Do not waste time and bookyour spot",
-    ],
-    images: ["/assets/images/portfolio/portfolio-big-01.jpg"],
-    thumbnail: "/assets/images/portfolio/portfolio-big-01.jpg",
-    ticket_img: "/assets/images/tickets/ticket.png",
-  },
-  {
-    title: "National Fest",
-    description: "National day of Bulgaria",
-    bgImage: "4",
-    date: "3.3.2023",
-    time: "20:00",
-    //Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
-    correctedDate: "",
-    correctedTime: "",
-    where: "Business Hall",
-    entry: 8,
-    memberEntry: 5,
-    price_id: "price_1MwA1eIOw5UGbAo12l33bqnE",
-    memberPrice_id: "price_1N0LPTIOw5UGbAo1UsHKyBQh",
-    text: [
-      "Wild party",
-      "We will provide drinks and snacks for our socity. Music and great spirit will crowd the dance floor as we promisethis will be an unforgetable experience that will be talked about for weeks after! Do not waste time and bookyour spot",
-    ],
-    images: ["/assets/images/portfolio/portfolio-big-01.jpg"],
-    thumbnail: "/assets/images/portfolio/portfolio-big-01.jpg",
-    ticket_img: "/assets/images/tickets/ticket.png",
-  },
+  // {
+  //   title: "Freedom Fest",
+  //   description: "National day of Bulgaria",
+  //   bgImage: "4",
+  //   date: "3.3.2023",
+  //   time: "20:00",
+  //   //Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
+  //   correctedDate: "",
+  //   correctedTime: "",
+  //   where: "Business Hall",
+  //   entry: 8,
+  //   memberEntry: 5,
+  //   price_id: "price_1MwA1eIOw5UGbAo12l33bqnE",
+  //   memberPrice_id: "price_1N0LPTIOw5UGbAo1UsHKyBQh",
+  //   text: [
+  //     "Wild party",
+  //     "We will provide drinks and snacks for our socity. Music and great spirit will crowd the dance floor as we promisethis will be an unforgetable experience that will be talked about for weeks after! Do not waste time and bookyour spot",
+  //   ],
+  //   images: ["/assets/images/portfolio/portfolio-big-01.jpg"],
+  //   thumbnail: "/assets/images/portfolio/portfolio-big-01.jpg",
+  //   ticket_img: "/assets/images/tickets/ticket.png",
+  // },
 ];
 
 const openNonSocietyEvents = [
