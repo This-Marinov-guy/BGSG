6cbd53f0cad496094d92c76af2c82e7266691b34
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-24 22:14:20 +0200

    modified:   src/util/serviceWorker.js

--- src/util/serviceWorker.js
+++ src/util/serviceWorker.js
@@ -23,6 +23,7 @@ const isLocalhost = Boolean(
 export function register(config) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.REACT_APP_URL}/service-worker.js`;
+      console.log(swUrl);
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
