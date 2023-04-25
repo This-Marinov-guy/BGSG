56ca1f6136c1fba74affcb37aa8381430792674b
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-25 10:47:14 +0200

    modified:   src/elements/ui/Update.jsx

--- src/elements/ui/Update.jsx
+++ src/elements/ui/Update.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
-import { FiX } from "react-icons/fi";
-import { useDispatch, useSelector } from "react-redux";
-import { removeWarning, selectWarning } from "../../redux/modal";
+import { useSelector } from "react-redux";
+import { selectWarning } from "../../redux/modal";
 import Modal from "react-bootstrap/Modal";
 
 import packageJson from "../../../package.json";
 
 const Update = () => {
-  const dispatch = useDispatch();
-
   const warning = useSelector(selectWarning);
-
-  const closeHandler = () => {
-    dispatch(removeWarning());
-  };
 
   return (
     <Modal
+      show={warning}
       size="md"
       aria-labelledby="contained-modal-title-vcenter"
-      show={warning}
+      freeze
     >
       <Alert className="error_panel" variant="info">
         <div className="action_btns">
           <h3>{"Version update  " + packageJson.version + " !"}</h3>
-          <FiX className="x_icon" onClick={closeHandler} />
         </div>
         <p>
           Your website is outdated! Please close all your running tabs with this
