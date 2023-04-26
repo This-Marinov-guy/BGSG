1f7b64f1132a61b571e4262ffbd9cb56d2dd6e98
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-26 11:23:38 +0200

    modified:   src/elements/ui/GoogleTranslate.jsx

--- src/elements/ui/GoogleTranslate.jsx
+++ src/elements/ui/GoogleTranslate.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 const GoogleTranslate = () => {
-  const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "en",
-        includedLanguages: "en,bg,nl",
-        layout: window.google.translate.TranslateElement.FloatPosition.TOP_LEFT,
-      },
-      "google_translate_element"
-    );
-  };
+  // const googleTranslateElementInit = () => {
+  //   new window.google.translate.TranslateElement(
+  //     {
+  //       pageLanguage: "en",
+  //       includedLanguages: "en,bg,nl",
+  //       layout: window.google.translate.TranslateElement.FloatPosition.TOP_LEFT,
+  //     },
+  //     "google_translate_element"
+  //   );
+  // };
 
-  useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
-  }, []);
+  // useEffect(() => {
+  //   var addScript = document.createElement("script");
+  //   addScript.setAttribute(
+  //     "src",
+  //     "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+  //   );
+  //   document.body.appendChild(addScript);
+  //   window.googleTranslateElementInit = googleTranslateElementInit;
+  // }, []);
 
   return (
     <li className="google_btn_item">
