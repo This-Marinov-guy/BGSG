84e4ffaadd31bff8ae633c0e69a5647234e3ebff
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-29 12:42:26 +0200

    modified:   src/util/serviceWorker.js

--- src/util/serviceWorker.js
+++ src/util/serviceWorker.js
@@ -57,6 +57,11 @@ function registerValidSW(swUrl, config) {
         }
       });
 
+      if (registration.waiting) {
+        console.log("Service worker already waiting, refreshing...");
+        registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      }
+
       registration.addEventListener("activate", () => {
         // get the current version of package.json
         fetch("/package.json")
