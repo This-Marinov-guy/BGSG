d9ba71f092c67d12288e3c6bbf7390a36ad1ca71
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-04-29 12:21:36 +0200

    modified:   src/pages/authentication/SignUp.jsx

--- src/pages/authentication/SignUp.jsx
+++ src/pages/authentication/SignUp.jsx
@@ -218,7 +218,6 @@ const SignUp = (props) => {
                     }
                   );
                   if (responseData.message === "verifiedKey") {
-                    formData.append("memberKey", values.memberKey);
                     try {
                       const responseData = await sendRequest(
                         `user/signup`,
