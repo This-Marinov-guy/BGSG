64a066bb02ff7ccf44afed0194eb2475f6d38154
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-05-02 10:01:04 +0200

    modified:   public/assets/images/avatars/bg_other_avatar_3.jpeg 	modified:   public/assets/images/avatars/bg_other_avatar_4.jpeg 	modified:   public/assets/images/avatars/bg_other_avatar_5.jpeg 	modified:   public/assets/images/avatars/bg_other_avatar_6.jpeg 	modified:   public/assets/images/brand/brand-01.png 	deleted:    public/assets/images/brand/brand-04.png 	deleted:    public/assets/images/brand/brand-05.png 	deleted:    public/assets/images/brand/brand-06.png 	modified:   src/elements/BrandTwo.jsx

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -8,10 +8,10 @@ class BrandTwo extends Component{
                 <ul className="brand-style-2">
                     
                     <li>
-                        <img src="/assets/images/brand/brand-03.png" alt="Logo Images"/>
+                        <img src="/assets/images/brand/brand-01.png" alt="Logo Images"/>
                     </li>
                     <li>
-                        <img src="/assets/images/brand/brand-04.png" alt="Logo Images"/>
+                        <img src="/assets/images/brand/brand-02.png" alt="Logo Images"/>
                     </li>
                 </ul>
             </React.Fragment>
