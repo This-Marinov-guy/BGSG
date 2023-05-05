af4d961df6ba6c7caa2a3fa89e4f3cccb9615062
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-05-05 12:01:43 +0200

    modified:   src/elements/Greeting.jsx

--- src/elements/Greeting.jsx
+++ src/elements/Greeting.jsx
@@ -9,14 +9,13 @@ const Greeting = () => {
             <LazyLoadImage className="card_img" src='/assets/images/news/george.jpg' alt='News' />
             <div className="card_text">
                 <div className="en">
-                    <h3>Happy Day of George</h3>
-                    <p>BGSG wishes you a happy holiday full of joy, great moments with family and friends and warm welcoming of summer! We wish all people named after the saint to be healthy and cheerful around the year and to be proud of their name!</p>
+                    <h3>Happy Day of St George</h3>
+                    <p>Bulgarian Society Groningen wishes you a happy holiday full of joy, great moments with your family and friends and a warm welcoming of the summer! We wish all people named after the Saint to be healthy and cheerful all year аround and to carry their name proudly!</p>
                     <p>...</p>
                 </div>
                 <div>
                     <h3>Честит Гергьовден</h3>
-                    <p>Ние от BGSG пожелаваме весело прекарване на празника, много весели емоции със семейството и близки и най-вече топло посрещане на лятото! А на всички именници - пожелаваме здраве, щастие, късмет и да си носят името със здраве!</p>
-                </div>
+                    <p>Ние от Bulgarian Society Groningen ви пожелаваме весело прекарване на празника, много позитивни емоции със семейството и близките ви и най-вече топло посрещане на лятото! А на всички именници - пожелаваме здраве, щастие, късмет и да си носят името със здраве!</p>                </div>
             </div>
         </div>)
 }
