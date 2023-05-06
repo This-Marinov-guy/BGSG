afd1b69f41f0eda8a25b3fde34ea84f24efb3adb
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-05-06 16:13:31 +0200

    modified:   public/assets/scss/elements/_pages.scss 	modified:   src/pages/information/Articles.jsx

--- src/pages/information/Articles.jsx
+++ src/pages/information/Articles.jsx
@@ -34,7 +34,7 @@ const Articles = () => {
         >
             <div className="inner-wrapper">
                 <div className="inner">
-                    <h2 style={{ textAlign: "start" }}>
+                    <h2 style={{ textAlign: "start", fontSize: '40px' }}>
                         The entrepreneurship series II: Toni Enchev Small steps
                         towards big goals
                     </h2>
