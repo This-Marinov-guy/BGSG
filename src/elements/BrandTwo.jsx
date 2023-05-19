79882fcdca5df8b07890f5d9c81a73c08c5e2b41
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-05-19 15:17:44 +0300

    modified:   public/assets/images/brand/brand-02.png 	new file:   public/assets/images/brand/brand-04.png 	modified:   src/elements/BrandTwo.jsx

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 class BrandTwo extends Component {
     render() {
         return (
-            <div className="rn-brand-area brand-separation bg_color--5 ptb--80">
+            <div className="rn-brand-area brand-separation bg_color--5 ptb--40">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
                             <h3 style={{ textAlign: 'center', fontFamily: 'Archive' }}>Partners</h3>
                             <ul className="brand-style-2">
-
                                 <li>
                                     <a href='https://en.unify.bg/' target='_blank'><img src="/assets/images/brand/brand-02.png" alt="Logo Images" /></a>
+                                </li>
+                                <li>
+                                    <a href='https://studybuddy.bg/' target='_blank'><img src="/assets/images/brand/brand-04.png" alt="Logo Images" /></a>
                                 </li>
                                 <li>
                                     <a href='https://www.stripe.com/' target='_blank'> <img src="/assets/images/brand/brand-03.png" alt="Logo Images" /></a>
