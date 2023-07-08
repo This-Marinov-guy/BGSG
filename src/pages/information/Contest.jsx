2f9d9a92ab9939302925b82d1ab93521a52907ff
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-07-08 19:26:37 +0300

    modified:   src/pages/information/Contest.jsx

--- src/pages/information/Contest.jsx
+++ src/pages/information/Contest.jsx
@@ -22,13 +22,20 @@ const Policy = React.memo(() => {
                     Short promo video contest organised by Bulgarian Society Groningen.        </h2>
             </div>
             <div className="container mt--80 mb--80">
-                <h3>1.Who can enter?
+                <h3>1. Who can enter?
                 </h3>
-                <p className="mt--10 mb--30">
+                <p className="mt--10 mb--50">
+                    Anyone can enter the contest,apart from the people who are part of the jury panel.
+                </p>
+                <h3>                2. What are the conditions?
+
+                </h3>
+
+                <p className="mt--10">
                     Submit a video with the following specifications:
 
                 </p>
-                <ul className="mt--10">
+                <ul style={{ fontSize: '24px' }}>
                     <li>0:30-1:00 min long
                     </li>
                     <li>Hasa background music
@@ -41,54 +48,56 @@ const Policy = React.memo(() => {
                     <li>Shows the achievements of BGSG
 
                     </li>
-                    <li>Uses the logo of BGSG</li>
+                    <li className="mb--50">Uses the logo of BGSG</li>
 
 
                 </ul>
                 <h3>3. Can I enter multiple times?
 
                 </h3>
-                <p className="mt--10 mb--30">
-                    Up to 2videos per person can enter the contest.
+                <p className="mt--10 mb--50">
+                    Up to 2 videos per person can enter the contest.
 
                 </p>
-                <h3 style={{ color: 'red' }}>5.What is the deadline?
+                <h3>4. How to enter the contest?
+
+
+                </h3>
+                <p className="mt--10 mb--50">
+                Submit the videos via WeTransfer or Google Drive to our email address: Bulgariansociety.gro@gmail.com
+
+                </p>
+                <h3 >5. What is the deadline?
 
 
 
                 </h3>
-                <p className="mt--10 mb--30">
+                <p style={{ color: 'red' }} className="mt--10 mb--50">
                     Deadline: 8 August 2023,23:59
 
                 </p>
-                <h3>6.When will I know the result?
+                <h3>6. When will I know the result?
                 </h3>
-                <p className="mt--10 mb--30">
+                <p className="mt--10 mb--50">
                     Results released on our Instagram channel on the <span style={{ color: 'green' }}>15th of August,2023at 13:00 (The winner will also be contacted via email.)</span>
                 </p>
-                <h3>1.Who can enter?
-                </h3>
-                <p className="mt--10 mb--30">
-                    Submit a video with the following specifications:
-
-                </p>
-                <h3>7.What is the prize?
+                <h3>7. What is the prize?
 
                 </h3>
-                <p className="mt--10 mb--30">
+                <p className="mt--10 mb--50">
                     The winner will receive a contribution of 50€ from Bulgarian Society Groningen
                 </p>
-                <h3>8.Who holds the rights to the video?
+                <h3>8. Who holds the rights to the video?
 
                 </h3>
-                <p className="mt--10 mb--30">
+                <p className="mt--10 mb--50">
                     Bulgarian Society Groningen reserves the right to use the submitted videos for the purpose of promoting the
                     organization.
                 </p>
                 <h3>9. Any other questions?
 
                 </h3>
-                <p className="mt--10 mb--30">
+                <p className="mt--10 mb--50">
                     Please contact us via email (bulgariansociety.gro@gmail.com) or through instagram (@bulgariansociety.gro)
 
                 </p>
