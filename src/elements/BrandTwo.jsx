b69335b5f5bc26c20343042210b1d9c764815983
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-10-15 17:17:09 +0300

    new file:   public/assets/images/brand/brand-08.jpg 	modified:   src/elements/BrandTwo.jsx

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -7,10 +7,13 @@ class BrandTwo extends Component {
                 <h3 style={{ textAlign: 'center', fontFamily: 'Archive' }}>Partners</h3>
                 <ul className="brand-style-2">
                     <li>
-                        <a href='https://www.cooltravel.bg/' target='_blank'><img className="splash-li"  src="/assets/images/brand/brand-06.png" alt="Logo Images" /></a>
+                        <a href='https://bulgariansocietyrtm.nl' target='_blank'><img src="/assets/images/brand/brand-08.jpg" style={{borderRadius: '50%'}} alt="Logo Images" /></a>
                     </li>
                     <li>
-                        <a href='https://domakin.nl/' target='_blank'><img src="/assets/images/brand/brand-07.png" alt="Logo Images" style={{borderRadius: '5%'}} /></a>
+                        <a href='https://www.cooltravel.bg/' target='_blank'><img className="splash-li" src="/assets/images/brand/brand-06.png" alt="Logo Images" /></a>
+                    </li>
+                    <li>
+                        <a href='https://domakin.nl/' target='_blank'><img src="/assets/images/brand/brand-07.png" alt="Logo Images" style={{ borderRadius: '5%' }} /></a>
                     </li>
                     <li>
                         <a href='https://studybuddy.bg/' target='_blank'><img src="/assets/images/brand/brand-04.png" alt="Logo Images" /></a>
@@ -21,7 +24,6 @@ class BrandTwo extends Component {
                     <li>
                         <a href='https://en.unify.bg/' target='_blank'><img src="/assets/images/brand/brand-02.png" alt="Logo Images" /></a>
                     </li>
-
                 </ul>
             </div>
 
